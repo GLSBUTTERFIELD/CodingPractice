e10cf9170a65fa1d998a7d9835504180be46d280
Gabby Butterfield <glsbutterfield@gmail.com>
2025-06-05 15:47:54 -0600

    reworked Caesar Cipher without using ASCII table and tested for accurate negative transversals

--- JavaCaesarCipher/src/CaesarCipher/CaesarCipher.java
+++ JavaCaesarCipher/src/CaesarCipher/CaesarCipher.java
@@ -1,58 +1,78 @@
 package CaesarCipher;
+
 import java.util.Scanner;
 
 public class CaesarCipher {
 
+	private static String lowerCase = "abcdefghijklmnopqrstuvwxyz";
+	private static String upperCase;
+
 	public static void main(String[] args) {
-		System.out.println("Enter text to encrypt: ");
 		Scanner sc = new Scanner(System.in);
+		upperCase = lowerCase.toUpperCase();
+
+		System.out.println("Enter text to encrypt: ");
 		String textInput = sc.nextLine();
+
 		System.out.println("Enter # of shifts (0-25): ");
 		int shiftInput = sc.nextInt();
 		sc.nextLine();
-		
-		System.out.println(encrypt(textInput, shiftInput));
-//		System.out.println(encrypt("Abc 10 LkxZe ne!a", 10));
-	}
-	
-	public static String encrypt (String text, int shift) {
-		StringBuilder result = new StringBuilder();
-		
-		for (char character : text.toCharArray()) {
-			if (Character.isLetter(character)) {
-				int charNum = (int)character;
-				int newNum = charNum + shift;
-					
-				if (Character.isLowerCase(character)) {
-					if (newNum > 'z') {
-						int remainder = newNum - 'z';
-						character = (char)('`' + remainder);
-					}
-					else {
-						character = (char)newNum;
-					}
-					
-				}
-				
-				else if (Character.isUpperCase(character) ) {
-					if (newNum - 'Z' >= 0) {
-						int remainder = newNum - 'Z';
-						character = (char) ('@' + remainder);
-					}
-					else {
-						character = (char)newNum;
-					}
-				}
 
+		System.out.println(encrypt(textInput, shiftInput));
+		sc.close();
+	}
+
+	public static String encrypt(String text, int shift) {
+		StringBuilder result = new StringBuilder();
+
+		for (char character : text.toCharArray()) {
+			String charSet = Character.isLowerCase(character) ? lowerCase : upperCase;
+			if (Character.isLetter(character)) {
+				int shiftedIndex = (charSet.indexOf(character) + shift) % 26;
+
+				if (shift < 0) {
+	
+					if (shift + charSet.indexOf(character) < 0) {
+						shiftedIndex = 26 + shiftedIndex;
+					}
+
+				}
+				result.append(charSet.charAt(shiftedIndex));
+
+			} else {
 				result.append(character);
-				
-			}
-			else {
-				result.append(character);
+
 			}
 		}
-		
+
 		return result.toString();
 	}
 
 }
+
+//				int charNum = (int)character;
+//				int newNum = charNum + shift;
+//					
+//				if (Character.isLowerCase(character)) {
+//					if (newNum > 'z') {
+//						int remainder = newNum - 'z';
+//						character = (char)('`' + remainder);
+//					}
+//					else {
+//						character = (char)newNum;
+//					}
+//					
+//				}
+//				
+//				else if (Character.isUpperCase(character) ) {
+//					if (newNum - 'Z' >= 0) {
+//						int remainder = newNum - 'Z';
+//						character = (char) ('@' + remainder);
+//					}
+//					else {
+//						character = (char)newNum;
+//					}
+//				}
+//
+//				result.append(character);
+//				
